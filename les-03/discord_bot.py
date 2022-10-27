edd16f116df196a0a8c0eed6137f84b616b65aec
martijn <35205@ma-web.nl>
2022-10-27 12:21:58 +0200

    bot push 2

--- les-03/discord_bot.py
+++ les-03/discord_bot.py
@@ -11,6 +11,21 @@ async def on_ready():
     guild = client.guilds[0]
     print(guild.name, "is te nae of the server")
     print(client.user, "has connected to the server")
+    channel = guild.text_channels[0]
+    print(channel.name, "is the name of the channel")
+    await channel.send("I'm online")
+
+@client.event
+async def on_message(message):
+    print(message.channel.name, "the message was posted from this channel")
+    print(message.content)
+    print(message.author, "is the user who wrote this")
+    print(message.created_at, "is when the message was posted")
+    print(message.channel, "is the channel this message was posted")
+    if message.author.bot == False:
+        await message.channel.send("Hello " + str(message.author))
+    
+    
 
 client.run(bottoken)
 
