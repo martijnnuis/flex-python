e7cb268084ff8f15b71f0f520ea71ce0d2eafcb0
martijn <35205@ma-web.nl>
2022-11-01 11:18:48 +0100

    readme update

--- les01/rndomtrt.py
+++ les01/rndomtrt.py
@@ -2,21 +2,32 @@ import turtle
 import random
 import time
 
-amount=(15)
+amount=(200)
 
-turtle.color('black')
+turtle.color('blue')
 turtle.speed(amount)
-for a in range(500):
+time.sleep(2)
+for a in range(976):
     turtle.forward(600)
-    turtle.left(149)
+    turtle.left(149.5)
+
 turtle.penup()
 turtle.home()
 turtle.pendown()
-turtle.color('lightblue')
+turtle.color('red')
 time.sleep(.2)
 
-for b in range(125):
+for b in range(244):
     turtle.forward(600)
     turtle.left(150.5)
+
+turtle.penup()
+turtle.home()
+turtle.pendown()
+turtle.color('green')
+
+for c in range(122):
+    turtle.forward(600)
+    turtle.left(150.25)
 
 turtle.done()
